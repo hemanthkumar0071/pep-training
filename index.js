31081d6b5d360de537aca516d34f72b69ae2929b
hemanthkumar0071 <hemanthjonnadula007@gmail.com>
2022-01-26 23:25:32 +0530

    training work

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log(a);
+console.log(b);
+
+let a=5;
+const b=5;
+
+
+
+//console.log(a);      //5
+//console.log(sum);    //10
+//console.log(m)   //undefined beacuse the func is assigned to a variable
+
+//var a=5;
+//var b=5;
+function sum(x,y){
+    var ans=x+y;
+    var m= multiply(ans);
+    return m;
+    return ans;
+}
+
+function multiply(z){
+    return 2*z;
+
+}
+
+var m= function multiply{
+    return 2*z;
+}
+var add= sum(a,b);
+console.log (add);
---
+++ namaste.js
@@ -0,0 +1,44 @@
+
+
+//var x=7;
+//function getName (){
+//    console.log("namaste javascript")
+//}
+
+//getName();
+//console.log(x);
+//console.log(getName);
+
+
+
+
+
+
+//var x=1;
+//a();
+//b();
+//console.log(x);
+
+
+//function a(){
+//    var x = 10;
+//    console.log(x);
+//
+//}
+
+//function b(){
+//    var x= 100;
+//    console.log(x);
+//}
+
+
+
+//the scope chain scope&lexical environment
+function a(){
+    console.log(b);
+}
+
+
+
+
+
